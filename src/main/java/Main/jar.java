b090f11732c7c19d706dd74a2d55dcfc40cd9c57
Iceknith <dimi.ma@laposte.net>
2021-01-08 20:42:38 +0100

    Initial commit

---
+++ src/main/java/Main/jar.java
@@ -0,0 +1,17 @@
+package Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class jar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

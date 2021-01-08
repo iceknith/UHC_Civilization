b70d86ea51bb6a1d1224007c4607e06bba896f41
Iceknith <dimi.ma@laposte.net>
2021-01-08 21:22:03 +0100

    changed the main class's name

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,7 +2,7 @@ package Main;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class jar extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {

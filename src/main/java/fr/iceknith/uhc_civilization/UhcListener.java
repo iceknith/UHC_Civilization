3e253cc39609596e952062c6bbc61109192172dd
lolocomotive <49951010+lolocomotive@users.noreply.github.com>
2021-03-17 00:56:49 +0100

    Add Javadoc

--- src/main/java/fr/iceknith/uhc_civilization/UhcListener.java
+++ src/main/java/fr/iceknith/uhc_civilization/UhcListener.java
@@ -1,5 +1,7 @@
 package fr.iceknith.uhc_civilization;
-
+/**
+ * The listener class used to listen for event (Unused for now)
+ */
 public class UhcListener implements org.bukkit.event.Listener {
 
 }
